e65edb8a0a7d5fb0f82ed0f9764be306eed948b7
mdsib <mdsiboldi@gmail.com>
2013-08-27 23:19:09 -0700

    index

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<?php
-	echo "hi ";
-?>
-hello
+<style> 
+#full {
+  background:gray;
+  min-height:100%;
+}
+</style>
+<div id="full">
+  <?php
+    echo "hi ";
+  ?>
+  hello
+</div>
+
